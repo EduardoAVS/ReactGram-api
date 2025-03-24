0289a21c76587057f7aecef3a5fc7da487a38ef2
EduardoAVS <eavsilva@sga.pucminas.br>
2025-03-24 15:41:36 -0300

    Rotas deletadas

--- api/routes/PhotosRoutes.ts
+++ api/routes/PhotosRoutes.ts
@@ -11,11 +11,11 @@ const router = express.Router();
 
 router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
 router.delete("/:id", authGuard, deletePhoto);
-router.get("/search", authGuard, searchPhotos);
+
 router.get("/", authGuard, getAllPhotos);
 router.get("/user/:id", authGuard, getUserPhotos);
 router.get("/:id", authGuard, getPhotoById);
-router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
+
 router.put("/like/:id", authGuard, likePhoto);
 router.put("/comment/:id", authGuard, commentValidation(), validate, commentPhoto)
 

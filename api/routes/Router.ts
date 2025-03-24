feb8f5c30f483282e63ef94a74b3fbdc1705e058
EduardoAVS <eavsilva@sga.pucminas.br>
2025-03-24 15:50:53 -0300

    PhotoRoutes deletado

--- api/routes/Router.ts
+++ api/routes/Router.ts
@@ -1,12 +1,12 @@
 import userRoutes from "./UserRoutes";
-import photosRoutes from "./PhotosRoutes" 
+
 import { Request, Response } from "express";
 import { Router } from "express";
 
 const router = Router();
 
 router.use("/api/users", userRoutes); 
-router.use("/api/photos", photosRoutes);
+
 
 // Teste router
 router.get("/", (req: Request, res: Response) => {
